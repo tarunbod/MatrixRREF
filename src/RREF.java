67588ba909341423942026bce5b004b013cc810d
Tarun Boddupalli <tarunbod@gmail.com>
2019-02-08 19:19:57 -0500

    Print in latex format

--- src/RREF.java
+++ src/RREF.java
@@ -16,7 +16,7 @@ public class RREF {
     }
 
     void solve() {
-//        printMatrix(false);
+        printMatrix();
         int pivotRow, pivotCol;
         for (pivotRow = 0; pivotRow < matrix.length; pivotRow++) {
             boolean found = false;
@@ -32,7 +32,7 @@ public class RREF {
                 }
             }
             if (found) {
-//                System.out.println("Using pivot column " + (pivotCol + 1));
+                System.out.println("Using pivot column " + (pivotCol + 1) + ";");
                 int swapRow = 0;
                 for (int i = pivotRow; i < matrix.length; i++) {
                     if (matrix[i][pivotCol] != 0) {
@@ -41,11 +41,11 @@ public class RREF {
                     }
                 }
                 if (swapRow != pivotRow) {
-//                    System.out.println("Swapping rows " + (swapRow + 1) + " and " + (pivotRow + 1));
+                    System.out.println("Swapping rows " + (swapRow + 1) + " and " + (pivotRow + 1) + ".");
                     double[] temp = Arrays.copyOf(matrix[pivotRow], matrix[0].length);
                     matrix[pivotRow] = matrix[swapRow];
                     matrix[swapRow] = temp;
-//                    printMatrix(false);
+                    printMatrix();
                 }
                 for (int i = pivotRow + 1; i < matrix.length; i++) {
                     if (matrix[i][pivotCol] != 0) {
@@ -53,14 +53,14 @@ public class RREF {
                         double[] pivotRowArr = Arrays.copyOf(matrix[pivotRow], matrix[0].length);
                         multiply(pivotRowArr, factor);
                         add(matrix[i], pivotRowArr);
-//                        System.out.println("Multiplying row " + (pivotRow + 1) + " by " + factor + "; adding result to row " + (i + 1));
-//                        printMatrix(false);
+                        System.out.println("Multiplying row " + (pivotRow + 1) + " by " + factor + "; adding result to row " + (i + 1) + ".");
+                        printMatrix();
                     }
                 }
             }
         }
-//        System.out.println("NOW IN ROW ECHELON FORM");
-//        printMatrix(true);
+        System.out.println("Now in row echelon form.");
+        printMatrix();
         pivotCol = -1;
         for (pivotRow = matrix.length - 1; pivotRow >= 0; pivotRow--) {
             for (int i = 0; i < matrix[0].length; i++) {
@@ -72,20 +72,20 @@ public class RREF {
             if (pivotCol != -1) {
                 double factor = 1 / matrix[pivotRow][pivotCol];
                 multiply(matrix[pivotRow], factor);
-//                System.out.println("Multiplying row " + (pivotRow + 1) + " by " + factor);
-//                printMatrix(false);
+                System.out.println("Multiplying row " + (pivotRow + 1) + " by " + factor + ".");
+                printMatrix();
                 for (int i = pivotRow - 1; i >= 0; i--) {
                     factor = -1 * matrix[i][pivotCol];
                     double[] pivotRowArr = Arrays.copyOf(matrix[pivotRow], matrix[0].length);
                     multiply(pivotRowArr, factor);
                     add(matrix[i], pivotRowArr);
-//                    System.out.println("Multiplying row " + (pivotRow + 1) + " by " + factor + "; adding result to row " + (i + 1));
-//                    printMatrix(false);
+                    System.out.println("Multiplying row " + (pivotRow + 1) + " by " + factor + "; adding result to row " + (i + 1) + ".");
+                    printMatrix();
                 }
             }
         }
-//        System.out.println("NOW IN REDUCED ROW ECHELON FORM:");
-//        printMatrix(true);
+       System.out.println("Now in reduced row echelon form.");
+       printMatrix();
     }
 
     private void multiply(double[] arr, double factor) {
@@ -100,44 +100,26 @@ public class RREF {
         }
     }
 
-    void printMatrix(boolean fancy) {
-        if (fancy) {
-            for (int i = 0; i < matrix[0].length * 10 + 1; i++) {
-                System.out.print("=");
-            }
-            System.out.println();
-        }
+    void printMatrix() {
+        System.out.println("\\begin{align*}\n\\begin{bmatrix}");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (fancy) {
-                    System.out.print("|| ");
+                System.out.printf("%6d ", (int) matrix[i][j]);
+                if (j != matrix[0].length - 1) {
+                    System.out.print("&");
                 }
-                System.out.printf("%6.2f ", matrix[i][j]);
             }
-            if (fancy) {
-                System.out.print("||");
-            }
-            System.out.println();
+            System.out.println("\\\\");
         }
-        if (fancy) {
-            for (int i = 0; i < matrix[0].length * 10 + 1; i++) {
-                System.out.print("=");
-            }
-            System.out.println();
-        }
+        System.out.println("\\end{bmatrix}\n\\end{align*}");
     }
 
     public static void main(String[] args) {
         RREF r = new RREF(new double[][]{
-//                {0, 0,  2, -4, -5,  2,  5},
-//                {0, 1, -1,  1,  3,  1, -1},
-//                {0, 6,  0, -6,  5, 16,  7}
-                {1, 2, -1, 2, 1, 2},
-                {-1, -2, 1, 2, 3, 6},
-                {2, 4, -3, 2, 0, 3},
-                {-3, -6, 2, 0, 3, 9}
+            {1,3,1,1,-1},
+            {-2,-6,-1,0,5},
+            {1,3,2,3,2}
         });
         r.solve();
-        r.printMatrix(true);
     }
 }
